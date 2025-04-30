b2e947f46baf2d5ee4f66cfbcb2f2ccd9606d00a
airidabu <airida.buinickaite@gmail.com>
2025-04-30 18:35:15 +0300

    feat: add authentication middleware to update user endpoint

--- api/userEndpoints.js
+++ api/userEndpoints.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const { register, login, updateUser } = require("../controllers/userController");
+const authMiddleware = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
-router.put("/update", updateUser);
+router.put("/update", authMiddleware, updateUser);
 
 module.exports = router;
