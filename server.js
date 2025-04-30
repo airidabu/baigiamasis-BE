3dca5ef39c2adc87cc2e0853f77c4d13729e987f
airidabu <airida.buinickaite@gmail.com>
2025-04-30 18:16:17 +0300

    feat: add CORS support to the application

--- server.js
+++ server.js
@@ -2,9 +2,16 @@ const express = require("express");
 const dotenv = require("dotenv");
 dotenv.config();
 require("./db");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+
+const userRoutes = require("./api/userEndpoints");
+
+app.use("/users", userRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
