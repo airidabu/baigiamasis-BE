dd0168af0c8de38165e077dd286c42e0dccf7215
airidabu <airida.buinickaite@gmail.com>
2025-04-30 19:03:57 +0300

    feat: add authentication middleware to get all users endpoint

--- api/userEndpoints.js
+++ api/userEndpoints.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.post("/register", register);
 router.post("/login", login);
 
-router.get("/", getAllUsers);
+router.get("/", authMiddleware, getAllUsers);
 router.put("/update/:id", authMiddleware, updateUser);
 
 module.exports = router;
